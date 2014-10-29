e8cd10fb72e0c817cf9b0ac853d297546ed987f0
Yongduek Seo <yndk@sogang.ac.kr>
2014-10-29 09:25:27 +0900

    backgroundMOG

---
+++ opencv_examples/videoHomographyTracking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  videoHomographyTracking
+//
+//  Created by Yongduek Seo on 10/22/14.
+//  Copyright (c) 2014 Yongduek Seo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ opencv_examples/video_homography/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  video_homography
+//
+//  Created by Yongduek Seo on 2014. 10. 20..
+//  Copyright (c) 2014년 Yongduek Seo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
