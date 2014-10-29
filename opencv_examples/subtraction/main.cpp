3a6be7a5a2c5adcbcf5d79352e7aaaf3c60d2e95
Yongduek Seo <yndk@sogang.ac.kr>
2014-10-29 09:44:44 +0900

    1

--- opencv_examples/subtraction/main.cpp
+++ opencv_examples/subtraction/main.cpp
@@ -7,9 +7,45 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    cv::VideoCapture camera;
+    int width, height;
+    
+    camera.open(0/*camera id*/);
+    // size change for real-time
+    camera.set(cv::CAP_PROP_FRAME_WIDTH, width=640);
+    camera.set(cv::CAP_PROP_FRAME_HEIGHT, height=480);
+    
+    cv::Mat frame;
+    cv::Mat_<uchar> gray(height,width), prevGray(height, width);
+    cv::Mat_<uchar> binary(height,width);
+    do {
+        camera >> frame;
+        if (frame.empty()) break;
+        cv::imshow ("frame", frame);
+        
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+        cv::Mat diff = gray - prevGray;
+        cv::Mat abs = cv::abs(diff);
+        cv::imshow ("absolute of diff", abs);
+        
+        float th = cv::threshold(abs, binary, 0, 255, cv::THRESH_OTSU | cv::THRESH_BINARY);
+        if (th<40.) {
+            th = 40.;
+            cv::threshold(abs, binary, th, 255, cv::THRESH_BINARY);
+        }
+        printf ("th= %f\n", th);
+        cv::imshow ("binary", binary);
+        
+        gray.copyTo (prevGray);
+        
+        if (cv::waitKey(1)==27) break;
+    } while (true);
+    
+
     return 0;
 }
--- opencv_examples/videoHomographyTracking/main.cpp
+++ opencv_examples/videoHomographyTracking/main.cpp
@@ -7,9 +7,85 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include "brisk.h"
+
+using namespace std;
+
+struct imageModel {
+    cv::Mat image;
+    std::vector<cv::KeyPoint> kpts;
+    cv::Mat desc;
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    string strVideoFilename = "video.mov";
+    cv::VideoCapture video, videofile;
+    int width, height;
+    
+    video.open(0/*camera id*/);
+    video.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    video.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    
+
+    videofile.open(strVideoFilename);
+    
+    if (videofile.isOpened()==false) {
+        cout << "video file open failed: " << strVideoFilename << endl;
+        return 0;
+    }
+    else {
+        width = videofile.get(cv::CAP_PROP_FRAME_WIDTH);
+        height = videofile.get(cv::CAP_PROP_FRAME_HEIGHT);
+        printf ("! video file size=%dx%d\n", width, height);
+    }
+
+        cv::BRISK featureDetector(10/*FAST threshold*/, 4/*detection octaves*/,1.0f/*pattern scale*/);
+//    cv::ORB featureDetector(1024 /*nMaxFeatures*/);
+//    cv::AKAZE featureDetector;
+
+    cv::Ptr<cv::DescriptorExtractor> dex;
+    dex = (cv::Ptr<cv::DescriptorExtractor>) new aaa::BriskDescriptorExtractor();
+    
+    cv::Mat displayImage;
+    
+    clock_t clockStart = clock();
+    unsigned frameCount=0;
+    
+    while (true) {
+        cv::Mat gray;
+        if (videofile.isOpened()) {
+            cv::Mat frame;
+            video >> frame;
+            if (frame.empty()) break;
+//            cv::resize(frame, frame, cv::Size(width/2,height/2));
+            cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+//            cv::resize(gray, gray, cv::Size(width/2,height/2));
+        }
+        
+        
+        vector<cv::KeyPoint> kpts;
+        cv::Mat descriptors;
+
+        featureDetector (gray, cv::noArray(), kpts, descriptors);
+        
+        featureDetector.detect(gray, kpts); //(gray, cv::noArray(), kpts, descriptors);
+//        featureDetector.compute(gray, kpts, descriptors);
+        
+        cv::drawKeypoints(gray, kpts, displayImage, cv::Scalar(100,5,5));
+//        cv::drawKeypoints(gray, kpts, displayImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    
+        float ellapsed = (clock()-clockStart)/(float)CLOCKS_PER_SEC;
+        string msg = "frameRate= " + to_string(++frameCount/ellapsed) + " fps "
+        + to_string(kpts.size()) + " points";
+        cv::putText(displayImage, msg, cv::Point(20,20), 1, 1, cv::Scalar(0,0,255));
+        cv::imshow ("display", displayImage);
+        
+        if (cv::waitKey(2)==27) break;
+    }
+    
+    video.release();
+    cout << "Finished.\n";
     return 0;
 }
